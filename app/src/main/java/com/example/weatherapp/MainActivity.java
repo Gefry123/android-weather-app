46058c922f0c927e308a1d2e3d3d5ec284fe02b2
mesuara <mesuara@gmail.com>
2023-06-27 21:33:28 -0400

    Made an API call

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.weatherapp;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText locationInput;
     private Spinner forecastSpinner;
     private String selectedForecast;
+    private TextView resultView;
+    String CITY;
+    String API = "59f089250c5fc563637f3af51b77123d";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         locationInput = findViewById(R.id.location_input);
         forecastSpinner = findViewById(R.id.forecast_spinner);
+        resultView = findViewById(R.id.result);
 
         forecastSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -35,9 +48,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void showWeather(View view) {
-        String location = locationInput.getText().toString();
+        CITY = locationInput.getText().toString();
+//        String myURL = "https://api.openweathermap.org/data/2.5/weather?q=" + CITY +API;
 
-        Intent intent;
+        new weatherTask().execute();
+//        Intent intent;
         if ("Today".equals(selectedForecast)) {
             // get weather for today
         } else {
@@ -46,5 +61,63 @@ public class MainActivity extends AppCompatActivity {
         // give location an start weather activity
 //        intent.putExtra("location", location);
 //        startActivity(intent);
+    }
+
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            /* Showing the ProgressBar, Making the main design GONE */
+            findViewById(R.id.loader).setVisibility(View.VISIBLE);
+            findViewById(R.id.errorText).setVisibility(View.GONE);
+        }
+
+        protected String doInBackground(String... args) {
+            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                Long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temp = main.getString("temp") + "°C";
+                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+                String pressure = main.getString("pressure");
+                String humidity = main.getString("humidity");
+
+                Long sunrise = sys.getLong("sunrise");
+                Long sunset = sys.getLong("sunset");
+                String windSpeed = wind.getString("speed");
+                String weatherDescription = weather.getString("description");
+
+                String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                /* Populating extracted data into our views */
+
+
+                resultView.setText(windSpeed);
+                /* Views populated, Hiding the loader, Showing the main design */
+                findViewById(R.id.loader).setVisibility(View.GONE);
+
+
+            } catch (JSONException e) {
+                findViewById(R.id.loader).setVisibility(View.GONE);
+                findViewById(R.id.errorText).setVisibility(View.VISIBLE);
+            }
+
+        }
     }
 }
