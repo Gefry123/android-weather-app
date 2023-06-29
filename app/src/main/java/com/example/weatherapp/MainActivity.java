52b21f7e9ad506fd1b8dbfb875e49f5bd15fb75f
Jeffrey Olivares <gefryquero27@gmail.com>
2023-06-29 03:11:19 -0400

    Added drawer with a settings and share option, you can change the preference in settings to fahrenheit or celsius

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,38 +1,61 @@
 package com.example.weatherapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.EditText;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import com.google.android.material.navigation.NavigationView;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText locationInput;
-    private Spinner forecastSpinner;
+    private AutoCompleteTextView locationInput;
+   // private Spinner forecastSpinner;
     private String selectedForecast;
     private TextView resultView;
+    ImageButton addButton;
     String CITY;
     String API = "59f089250c5fc563637f3af51b77123d";
+    String myURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+    //DRAWER NAVIGATION VARIABLES
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    //TEMPERATURE UNIT
+    private String temperature_unit;
+    private DecimalFormat df = new DecimalFormat("#.##");
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        addButton = findViewById(R.id.add_button);
         locationInput = findViewById(R.id.location_input);
-       // forecastSpinner = findViewById(R.id.forecast_spinner);
-       // resultView = findViewById(R.id.result);
+
+        // forecastSpinner = findViewById(R.id.forecast_spinner);
+        // resultView = findViewById(R.id.result);
 
    /*   forecastSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -44,12 +67,74 @@ public class MainActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });*/
+
+        //ADD BUTTON CODE FOR LISTENERS --- SENT VALUES TO WeatherActivity
+        addButton.setOnClickListener(v -> {
+            CITY = locationInput.getText().toString().trim();
+            Intent intent = new Intent(this, WeatherActivity.class);
+            intent.putExtra("CityName", CITY);
+            startActivity(intent);
+        });
+
+        //DRAWER LAYOUT CODE
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.navitagionView);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.menu_open,
+                R.string.menu_close
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //SET NAVIGATION LISTENERS
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // Handle navigation item selection here
+
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_notifications) {
+                // Handle notifications item selection
+            } else if (itemId == R.id.nav_settings) {
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (itemId == R.id.nav_share) {
+                // Handle share item selection
+
+            }
+
+            drawerLayout.closeDrawers();
+            return true;
+        });
+        //CODE FOR PREFERENCES TO CHANGE CELSIUS TO FAHRENHEIT
+        // Load the preferences from the XML resource
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        //Get the stored temperature preference
+        temperature_unit = sharedPreferences.getString("temperature", "");
+        //Set the temperature based on selected preference
+
     }
-    public void showWeather(View view) {
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+   /* public void showWeather(View view) {
         CITY = locationInput.getText().toString().trim();
         String myURL = "https://api.openweathermap.org/data/2.5/weather?q=" + CITY + API;
-
-//        new weatherTask().execute();
+     // new weatherTask().execute();
 
         if ("Today".equals(selectedForecast)) {
             Intent intent = new Intent(this, WeatherActivity.class);
@@ -129,5 +214,6 @@ public class MainActivity extends AppCompatActivity {
 //
 //        }
 //    }
+
 
 }
